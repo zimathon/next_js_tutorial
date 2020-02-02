3196dbd31d23e1ea289e45b367bc00a198132388
zimathon <zimathon@gmail.com>
2020-02-02 22:14:55 +0900

    until API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,72 +1,48 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
+import useSWR from 'swr';
 
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
 }
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${post.id}`}>
-      <a>{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
+export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
 
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: 'Arial';
-      }
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
 
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-);
-
-
-export default function Blog() {
   return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        {getPosts().map(post => (
-          <PostLink key={post.id} post={post} />
-        ))}
-      </ul>
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
+
       <style jsx>{`
-        h1,
-        a {
-          font-family: 'Arial';
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
         }
-
-        ul {
-          padding: 0;
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
         }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
         }
       `}</style>
-    </Layout>
+    </main>
   );
 }
